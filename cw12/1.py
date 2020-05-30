c51805a255bebac1d87eb4eca3ea708a1327320f
MattBUWM <155671@student.uwm.edu.pl>
2020-05-30 18:55:12 +0200

    zad1-5

--- cw12/1.py
+++ cw12/1.py
@@ -5,7 +5,7 @@ url = "https://boardgamegeek.com"
 data = requests.get(url)
 page = html.fromstring(data.text)
 
-#chodźby nie wiem co próbuję nic nie chce działać, nawet jak bezpośrednio z przeglądarki skopiuję xpath dla elementów <a>
+#chodźby nie wiem co próbuję nie chce działać jak powinno, nawet jak bezpośrednio z przeglądarki skopiuję xpath dla elementów <a>
 for x in range (2,6+2+1):
     xpath='//*[@id="results_1"]/div/div/div[2]/div['+str(x)+']/h2/a'
     foundElement = page.xpath(xpath)
